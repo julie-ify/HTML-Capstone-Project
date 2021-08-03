3f89e8fcbfa73aed9c116ff8877a563dc4312a95
Juliana Ifionu <julieifionu@gmail.com>
2021-08-03 10:49:28 +0100

    Add side menu dynamically when the user clicks the hamburger menu, side menu appears and disappears when user clicks on the (X) button

---
+++ menu.js
@@ -0,0 +1,24 @@
+const openMenu = document.getElementById('menu');
+const closeMenu = document.getElementById('close');
+const Menu = document.getElementById('side-menu');
+
+let sideMenu = false;
+
+function openSideMenu() {
+  Menu.style.display = 'flex';
+  sideMenu = true;
+}
+
+function closeSideMenu() {
+  Menu.style.display = 'none';
+  sideMenu = false;
+}
+
+function getMenu() {
+  if (sideMenu === false) {
+    openSideMenu();
+  } else closeSideMenu();
+}
+
+openMenu.addEventListener('click', openSideMenu);
+closeMenu.addEventListener('click', closeSideMenu);

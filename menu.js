4837562c00984d7eedfa683ceccf23f5d00e643c
Juliana Ifionu <julieifionu@gmail.com>
2021-08-04 19:08:19 +0100

    Add scroll to the top button using JavaScript

--- menu.js
+++ menu.js
@@ -26,3 +26,36 @@ closeMenu.addEventListener('click', closeSideMenu);
 if (getMenu === 0) {
   getMenu();
 }
+
+// Arrow button
+
+const arrow = document.getElementById('arrow');
+const arrowIcon = document.getElementById('down');
+arrowIcon.style.display = 'flex';
+arrowIcon.style.alignItems = 'center';
+arrowIcon.style.justifyContent = 'center';
+arrow.style.position = 'fixed';
+arrow.style.bottom = '1rem';
+arrow.style.right = '1rem';
+arrow.style.borderRadius = '50%';
+arrow.style.color = '#ec5242';
+arrow.style.padding = '5px 15px';
+arrow.style.border = '2px solid #ec5242';
+arrow.style.width = '1px';
+arrow.style.cursor = 'pointer';
+arrow.style.display = 'none';
+arrow.style.transition = '300ms';
+
+function getArrow() {
+  if (
+    document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 100) {
+    arrow.style.display = 'block';
+  } else arrow.style.display = 'none';
+}
+
+window.onscroll = getArrow;
+function getTop() {
+  document.documentElement.scrollTop = 0;
+}
+
+arrow.addEventListener('click', getTop);

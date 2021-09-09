2e1f60b1b11c13de9e916348b907db22c7ce7474
Juliana Ifionu <julieifionu@gmail.com>
2021-09-09 21:17:05 +0100

    Fix linter error

--- main.js
+++ main.js
@@ -29,37 +29,37 @@ const speaker1 = new Speakers(
   'images/speaker-1.png',
   'Quincy Ememandu',
   'Proffesional Internet Marketer and Digital marketer',
-  'Quincy has been in digital market industry for 10 years and has been invited to speak on conferences all over the world.'
+  'Quincy has been in digital market industry for 10 years and has been invited to speak on conferences all over the world.',
 );
 const speaker2 = new Speakers(
   'images/speaker-2.png',
   'Teddy Livingstone',
   'Professional Ruby on rail developer',
-  'Teddy has been in software industry for 7 years and has been invited to speak on conferences all over the world.'
+  'Teddy has been in software industry for 7 years and has been invited to speak on conferences all over the world.',
 );
 const speaker3 = new Speakers(
   'images/speaker-3.png',
   'Freedom Dozie',
   'Professional Business manager and retired software developer',
-  'Freedom has been in software industry for 7 years and has been invited to speak on conferences all over the world.'
+  'Freedom has been in software industry for 7 years and has been invited to speak on conferences all over the world.',
 );
 const speaker4 = new Speakers(
   'images/speaker-4.png',
   'Emily Suarez',
   'Professional Community Manager',
-  'Emily has been a community manager for 7 years and has been invited to speak on conferences all over the world.'
+  'Emily has been a community manager for 7 years and has been invited to speak on conferences all over the world.',
 );
 const speaker5 = new Speakers(
   'images/speaker-5.png',
   'Mary Ruben',
   'Professional JavaScript and React developer',
-  'has been a developer for 7 years and has been invited to speak on conferences all over the world.'
+  'has been a developer for 7 years and has been invited to speak on conferences all over the world.',
 );
 const speaker6 = new Speakers(
   'images/speaker-6.png',
   'Julie May',
   'Professional Full stack Software Developer',
-  'has been a developer for 7 years and has been invited to speak on conferences all over the world.'
+  'has been a developer for 7 years and has been invited to speak on conferences all over the world.',
 );
 
 const speakerArr = [speaker1, speaker2, speaker3, speaker4, speaker5, speaker6];
@@ -91,7 +91,6 @@ const renderSpeaker = () => {
     list.appendChild(div1);
     list.appendChild(div2);
     speakerList.appendChild(list);
-    
   }
   speakerList.appendChild(addMore);
 };
@@ -130,10 +129,9 @@ const renderRestSpeaker = () => {
 
     addMore.style.display = 'none';
 
-    const closeList = document.querySelector('.list2');
     seeLess.addEventListener('click', () => {
       speakerList.innerHTML = '';
-      renderSpeaker()
+      renderSpeaker();
       addMore.style.display = 'flex';
     });
   });

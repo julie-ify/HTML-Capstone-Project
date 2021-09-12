1eb4e1892c777dd0e440852d01d41298b93ce820
Juliana Ifionu <julieifionu@gmail.com>
2021-09-12 13:21:26 +0100

    Display all speakers on large screen

--- main.js
+++ main.js
@@ -29,124 +29,59 @@ const speaker1 = new Speakers(
   'images/speaker-1.png',
   'Quincy Ememandu',
   'Proffesional Internet Marketer and Digital marketer',
-  'Quincy has been in digital market industry for 10 years and has been invited to speak on conferences all over the world.',
+  'Quincy has been in digital market industry for 10 years and has been invited to speak on conferences all over the world.'
 );
 const speaker2 = new Speakers(
   'images/speaker-2.png',
   'Teddy Livingstone',
   'Professional Ruby on rail developer',
-  'Teddy has been in software industry for 7 years and has been invited to speak on conferences all over the world.',
+  'Teddy has been in software industry for 7 years and has been invited to speak on conferences all over the world.'
 );
 const speaker3 = new Speakers(
   'images/speaker-3.png',
   'Freedom Dozie',
   'Professional Business manager and retired software developer',
-  'Freedom has been in software industry for 7 years and has been invited to speak on conferences all over the world.',
+  'Freedom has been in software industry for 7 years and has been invited to speak on conferences all over the world.'
 );
 const speaker4 = new Speakers(
   'images/speaker-4.png',
   'Emily Suarez',
   'Professional Community Manager',
-  'Emily has been a community manager for 7 years and has been invited to speak on conferences all over the world.',
+  'Emily has been a community manager for 7 years and has been invited to speak on conferences all over the world.'
 );
 const speaker5 = new Speakers(
   'images/speaker-5.png',
   'Mary Ruben',
   'Professional JavaScript and React developer',
-  'has been a developer for 7 years and has been invited to speak on conferences all over the world.',
+  'has been a developer for 7 years and has been invited to speak on conferences all over the world.'
 );
 const speaker6 = new Speakers(
   'images/speaker-6.png',
   'Julie May',
   'Professional Full stack Software Developer',
-  'has been a developer for 7 years and has been invited to speak on conferences all over the world.',
+  'has been a developer for 7 years and has been invited to speak on conferences all over the world.'
 );
 
 const speakerArr = [speaker1, speaker2, speaker3, speaker4, speaker5, speaker6];
 
-speakerList.innerHTML = '';
-const fullSpeaker = () => {
+window.addEventListener('load', () => {
   speakerList.innerHTML = '';
-  for (let i = 0; i < speakerArr.length; i += 1) {
-    const list = document.createElement('li');
-    list.classList.add('speaker-item');
-    const img = document.createElement('img');
-    img.setAttribute('src', speakerArr[i].img);
-    const name = document.createElement('h4');
-    name.textContent = speakerArr[i].name;
-    const italics = document.createElement('i');
-    italics.textContent = speakerArr[i].title;
-    const title = document.createElement('p');
-    title.classList.add('colored-text');
-    title.appendChild(italics);
-    const description = document.createElement('p');
-    description.textContent = speakerArr[i].description;
-    const div1 = document.createElement('div');
-    const hr = document.createElement('hr');
-    title.appendChild(hr);
-    const div2 = document.createElement('div');
-    div1.appendChild(img);
-    div2.appendChild(name);
-    div2.appendChild(title);
-    div2.appendChild(description);
-    list.appendChild(div1);
-    list.appendChild(div2);
-    speakerList.appendChild(list);
-  }
-};
-
-const renderSpeaker = () => {
-  speakerList.innerHTML = '';
-  const firstHalfSpeaker = speakerArr.slice(0, 3);
-  for (let i = 0; i < firstHalfSpeaker.length; i += 1) {
-    const list = document.createElement('li');
-    list.classList.add('speaker-item');
-    const img = document.createElement('img');
-    img.setAttribute('src', firstHalfSpeaker[i].img);
-    const name = document.createElement('h4');
-    name.textContent = firstHalfSpeaker[i].name;
-    const italics = document.createElement('i');
-    italics.textContent = firstHalfSpeaker[i].title;
-    const title = document.createElement('p');
-    title.classList.add('colored-text');
-    title.appendChild(italics);
-    const description = document.createElement('p');
-    description.textContent = firstHalfSpeaker[i].description;
-    const div1 = document.createElement('div');
-    const hr = document.createElement('hr');
-    title.appendChild(hr);
-    const div2 = document.createElement('div');
-    div1.appendChild(img);
-    div2.appendChild(name);
-    div2.appendChild(title);
-    div2.appendChild(description);
-    list.appendChild(div1);
-    list.appendChild(div2);
-    speakerList.appendChild(list);
-  }
-  speakerList.appendChild(addMore);
-  
-};
-
-const renderRestSpeaker = () => {
- 
-  addMore.addEventListener('click', () => {
-    const secondHalfSpeaker = speakerArr.slice(3);
-    for (let i = 0; i < secondHalfSpeaker.length; i += 1) {
-      const list2 = document.createElement('li');
-      list2.classList.add('speaker-item');
-      list2.classList.add('list2');
+  const fullSpeaker = () => {
+    speakerList.innerHTML = '';
+    for (let i = 0; i < speakerArr.length; i += 1) {
+      const list = document.createElement('li');
+      list.classList.add('speaker-item');
       const img = document.createElement('img');
-      img.setAttribute('src', secondHalfSpeaker[i].img);
+      img.setAttribute('src', speakerArr[i].img);
       const name = document.createElement('h4');
-      name.textContent = secondHalfSpeaker[i].name;
+      name.textContent = speakerArr[i].name;
       const italics = document.createElement('i');
-      italics.textContent = secondHalfSpeaker[i].title;
+      italics.textContent = speakerArr[i].title;
       const title = document.createElement('p');
       title.classList.add('colored-text');
       title.appendChild(italics);
       const description = document.createElement('p');
-      description.textContent = secondHalfSpeaker[i].description;
+      description.textContent = speakerArr[i].description;
       const div1 = document.createElement('div');
       const hr = document.createElement('hr');
       title.appendChild(hr);
@@ -155,34 +90,99 @@ const renderRestSpeaker = () => {
       div2.appendChild(name);
       div2.appendChild(title);
       div2.appendChild(description);
-      list2.appendChild(div1);
-      list2.appendChild(div2);
-      speakerList.appendChild(list2);
-      speakerList.appendChild(seeLess);
+      list.appendChild(div1);
+      list.appendChild(div2);
+      speakerList.appendChild(list);
     }
+  };
 
-    addMore.style.display = 'none';
+  const renderSpeaker = () => {
+    speakerList.innerHTML = '';
+    const firstHalfSpeaker = speakerArr.slice(0, 3);
+    for (let i = 0; i < firstHalfSpeaker.length; i += 1) {
+      const list = document.createElement('li');
+      list.classList.add('speaker-item');
+      const img = document.createElement('img');
+      img.setAttribute('src', firstHalfSpeaker[i].img);
+      const name = document.createElement('h4');
+      name.textContent = firstHalfSpeaker[i].name;
+      const italics = document.createElement('i');
+      italics.textContent = firstHalfSpeaker[i].title;
+      const title = document.createElement('p');
+      title.classList.add('colored-text');
+      title.appendChild(italics);
+      const description = document.createElement('p');
+      description.textContent = firstHalfSpeaker[i].description;
+      const div1 = document.createElement('div');
+      const hr = document.createElement('hr');
+      title.appendChild(hr);
+      const div2 = document.createElement('div');
+      div1.appendChild(img);
+      div2.appendChild(name);
+      div2.appendChild(title);
+      div2.appendChild(description);
+      list.appendChild(div1);
+      list.appendChild(div2);
+      speakerList.appendChild(list);
+    }
+    speakerList.appendChild(addMore);
+  };
 
-    seeLess.addEventListener('click', () => {
+  const renderRestSpeaker = () => {
+    addMore.addEventListener('click', () => {
+      const secondHalfSpeaker = speakerArr.slice(3);
+      for (let i = 0; i < secondHalfSpeaker.length; i += 1) {
+        const list2 = document.createElement('li');
+        list2.classList.add('speaker-item');
+        list2.classList.add('list2');
+        const img = document.createElement('img');
+        img.setAttribute('src', secondHalfSpeaker[i].img);
+        const name = document.createElement('h4');
+        name.textContent = secondHalfSpeaker[i].name;
+        const italics = document.createElement('i');
+        italics.textContent = secondHalfSpeaker[i].title;
+        const title = document.createElement('p');
+        title.classList.add('colored-text');
+        title.appendChild(italics);
+        const description = document.createElement('p');
+        description.textContent = secondHalfSpeaker[i].description;
+        const div1 = document.createElement('div');
+        const hr = document.createElement('hr');
+        title.appendChild(hr);
+        const div2 = document.createElement('div');
+        div1.appendChild(img);
+        div2.appendChild(name);
+        div2.appendChild(title);
+        div2.appendChild(description);
+        list2.appendChild(div1);
+        list2.appendChild(div2);
+        speakerList.appendChild(list2);
+        speakerList.appendChild(seeLess);
+      }
+
+      addMore.style.display = 'none';
+
+      seeLess.addEventListener('click', () => {
+        speakerList.innerHTML = '';
+        renderSpeaker();
+        addMore.style.display = 'flex';
+      });
+    });
+  };
+
+  const smallDevice = window.matchMedia('(min-width: 900px)');
+
+  function handleDeviceChange(e) {
+    if (e.matches) {
+      fullSpeaker();
+    } else {
       speakerList.innerHTML = '';
       renderSpeaker();
-      addMore.style.display = 'flex';
-    });
-  });
-};
-
-const smallDevice = window.matchMedia('(min-width: 900px)');
-
-function handleDeviceChange(e) {
-  if (e.matches) {
-    fullSpeaker();
-  } else {
-    speakerList.innerHTML = '';
-    renderSpeaker();
-    renderRestSpeaker()
+      renderRestSpeaker();
+    }
   }
-}
 
-smallDevice.addListener(handleDeviceChange);
-// Run it initially
-handleDeviceChange(smallDevice);
+  smallDevice.addListener(handleDeviceChange);
+  // Run it initially
+  handleDeviceChange(smallDevice);
+});
